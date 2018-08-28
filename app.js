e5634451776dd08bdf7e3df91f35205be256bbc0
leviboenish <leviboenish@gmail.com>
2018-08-28 09:43:41 -0600

    fixing body parser

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const queries = require('./queries');
 
